3548d57788170828971b905cb9aad9e1eae955bc
Jarome Leslie <jsleslie@users.noreply.github.com>
2020-05-02 19:46:04 -0700

    download tail nums

---
+++ src/download_tailnums.py
@@ -0,0 +1,43 @@
+# Author: Jarome Leslie
+
+
+import requests
+from selenium import webdriver
+import pandas as pd
+
+def extract_tail_info(tails):
+    driver = webdriver.Chrome()
+    result = {'tail_num': [], 'manufacturer': [], 'model': []}
+
+    failures = {'tail_num': []}
+    count =0
+    driver = webdriver.Chrome()
+    for tail in tails:
+
+        try:
+            tail = str(tail)
+            url_ = 'https://registry.faa.gov/aircraftinquiry/NNum_Results.aspx?NNumbertxt='
+            driver.get(url_+tail)            
+            result['manufacturer'].append(driver.find_element_by_id("ctl00_content_lbMfrName").text)
+            result['model'].append(driver.find_element_by_id("ctl00_content_Label7").text)
+            result['tail_num'].append(tail)
+            count += 1
+            print(count)
+        except:
+            failures['tail_num'].append(tail)
+
+    pd.DataFrame(result).to_csv('data/tailnums.csv')
+    pd.DataFrame(failures).to_csv('data/missing_tailnums.csv')
+    
+    
+    
+def main():
+    
+    load_ord = pd.read_csv('../data/ORD_OTP.csv')
+    tail_nums = load_ord['TAIL_NUM'].unique()
+    extract_tail_info(tail_nums)
+    
+
+# script entry point
+if __name__ == '__main__':
+    main()    
